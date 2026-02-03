0f815d75f68757450b8a61ccc027a3a3e345e3a0
Perel <pn0556781949@gmail.com>
2026-02-03 15:27:57 +0200

    1

--- server.js
+++ server.js
@@ -1,33 +1,17 @@
-require("dotenv").config();
-const express = require("express");
-const cors = require("cors");
-const http = require("http");
-const socketIo = require("socket.io");
+// var mysql = require("mysql2");
+var path = require("path");
+require("dotenv").config({ path: path.resolve(__dirname, "../../.env") }); // בדיקת טעינת המשתנים הסביבתיים
+// const conDB = require("./src/DB/connectToDB");
 
-const app = express();
-const server = http.createServer(app);
-const io = socketIo(server, {
-  cors: { origin: "*" }, // מאפשר חיבורים מכל מקום
-});
-app.use(cors());
-app.use(express.json()); // מאפשר שליחת JSON בבקשות
+function cleanDB() {
+  conDB.query(
+    "DROP DATABASE IF EXISTS " + "railway",
+    function (err, result) {
+      if (err) throw err;
+      console.log("Database dropped");
+    }
+  );
+}
 
-// נתיבים לדוגמה
-app.use("/users", require("./src/API/users.routes"));
-app.use("/apartments", require("./src/API/apartments.routes"));
-app.use("/donations", require("./src/API/donations.routes")(io));
-io.on("connection", (socket) => {
-  console.log("לקוח מחובר");
-
-  socket.on("disconnect", () => {
-    console.log("לקוח התנתק");
-  });
-});
-app.get("", (req, res) => {
-  return res.status(200).json("maching server");
-});
-
-const PORT = process.env.PORT || 8080;
-server.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
-});
+cleanDB();
+module.exports = cleanDB;
