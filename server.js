6db61dcec99eb5e23448f55d587df5b0bd450d05
Perel <pn0556781949@gmail.com>
2026-02-03 16:50:05 +0200

    5

--- server.js
+++ server.js
@@ -1,69 +1,33 @@
-const resetDB = require("./src/DB/resetDB");
-const createDB = require("./src/DB/createDB");
-const createTables = require("./src/DB/createTables");
-const initialization = require("./src/DB/initialization");
-async function startup() {
-  console.log(`startup`);
-  await resetDB();
-  console.log(`resetDB`);
-  await createDB();
-  console.log(`createDB`);
-  await createTables();
-  console.log(`createTables`);
-  await initialization();
-  console.log(`initialization`);
-}
-startup();
-function TreeNode(data) {
-  this.data = data;
-  this.left = null;
-  this.right = null;
-}
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const http = require("http");
+const socketIo = require("socket.io");
 
-function f(A) {
-  let index;
-  // let matrix = new Array(A + 1).fill([]).map(() => new Array(A + 1).fill([]));
-  let matrix = new Array(A + 1).fill(null).map(() => new Array(A + 1).fill(null).map(() => []));
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server, {
+  cors: { origin: "*" }, // מאפשר חיבורים מכל מקום
+});
+app.use(cors());
+app.use(express.json()); // מאפשר שליחת JSON בבקשות
 
-  for (let i = 0; i < A; i++)
-    for (let j = 1; j <= A - i; j++) {
-      index = 0;
-      for (let k = j; k <= j + i; k++) {
-        console.log(k, j, i);
+// נתיבים לדוגמה
+app.use("/users", require("./src/API/users.routes"));
+app.use("/apartments", require("./src/API/apartments.routes"));
+app.use("/donations", require("./src/API/donations.routes")(io));
+io.on("connection", (socket) => {
+  console.log("לקוח מחובר");
 
-        if (i == 0) matrix[j][j] = [new TreeNode(k)];
-        if (k == j + i) {
-          for (let y = 0; y < matrix[j][k - 1].length; y++) {
-            console.log(`in just left tree k=${k} index=${index}`)
-            matrix[j][j + i][index] = new TreeNode(k);
-            matrix[j][j + i][index++].left = matrix[j][k - 1][y];
-          }
-        } else {
-          if (k == j) {
-            for (let x = 0; x < matrix[k + 1][j + i].length; x++) {
-              ///let rightTree of matrix[k + 1][j + i]
-              console.log(`in just right tree k=${k} index=${index}`)
-              matrix[j][j + i][index] = new TreeNode(k);
-              matrix[j][j + i][index++].right = matrix[k + 1][j + i][x];
-            }
-          }
+  socket.on("disconnect", () => {
+    console.log("לקוח התנתק");
+  });
+});
+app.get("", (req, res) => {
+  return res.status(200).json("maching server");
+});
 
-          for (let y = 0; y < matrix[j][k - 1].length; y++) {
-            //let leftTree of matrix[j][k - 1]
-            console.log(k, j, i);
-            // console.log(matrix[k + 1][j + i]);
-
-            for (let x = 0; x < matrix[k + 1][j + i].length; x++) {
-              matrix[j][j + i][index] = new TreeNode(k);
-              matrix[j][j + i][index].left = matrix[j][k - 1][y];
-              matrix[j][j + i][index++].right = matrix[k + 1][j + i][x];
-            }
-          }
-        }
-      }
-      console.log(matrix[j][j + i]);
-    }
-  return matrix[1][A];
-}
-const result = f(3);
-console.log(result);
+const PORT = process.env.PORT || 8080;
+server.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
