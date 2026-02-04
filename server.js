89c566e45ef95e5533ac213d9e00b8aaf44d9946
Perel <pn0556781949@gmail.com>
2026-02-04 13:42:22 +0200

    8

--- server.js
+++ server.js
@@ -1,33 +1,11 @@
-require("dotenv").config();
-const express = require("express");
-const cors = require("cors");
-const http = require("http");
-const socketIo = require("socket.io");
+// const conDB = require("./connectToDB");
 
-const app = express();
-const server = http.createServer(app);
-const io = socketIo(server, {
-  cors: { origin: "*" }, // מאפשר חיבורים מכל מקום
-});
-app.use(cors());
-app.use(express.json()); // מאפשר שליחת JSON בבקשות
-
-// נתיבים לדוגמה
-app.use("/users", require("./src/API/users.routes"));
-app.use("/apartments", require("./src/API/apartments.routes"));
-app.use("/donations", require("./src/API/donations.routes")(io));
-io.on("connection", (socket) => {
-  console.log("לקוח מחובר");
-
-  socket.on("disconnect", () => {
-    console.log("לקוח התנתק");
+function deleteUsersTable() {
+  conDB.query("DROP TABLE IF EXISTS users", function (err, result) {
+    if (err) throw err;
+    console.log("Users table deleted");
   });
-});
-app.get("", (req, res) => {
-  return res.status(200).json("maching server");
-});
+}
 
-const PORT = process.env.PORT || 8080;
-server.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
-});
+deleteUsersTable();
+module.exports = deleteUsersTable;
