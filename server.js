a4e0c8347b6e3ab2594ce2691891b50f82535f71
Perel <pn0556781949@gmail.com>
2026-02-05 03:17:06 +0200

    19

--- server.js
+++ server.js
@@ -3,32 +3,31 @@ const express = require("express");
 const cors = require("cors");
 const http = require("http");
 const socketIo = require("socket.io");
-import createTables from "src/DB/createTables";
-createTables()
-// const app = express();
-// const server = http.createServer(app);
-// const io = socketIo(server, {
-//   cors: { origin: "*" }, // מאפשר חיבורים מכל מקום
-// });
-// app.use(cors());
-// app.use(express.json()); // מאפשר שליחת JSON בבקשות
 
-// // נתיבים לדוגמה
-// app.use("/users", require("./src/API/users.routes"));
-// app.use("/apartments", require("./src/API/apartments.routes"));
-// app.use("/donations", require("./src/API/donations.routes")(io));
-// io.on("connection", (socket) => {
-//   console.log("לקוח מחובר");
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server, {
+  cors: { origin: "*" }, // מאפשר חיבורים מכל מקום
+});
+app.use(cors());
+app.use(express.json()); // מאפשר שליחת JSON בבקשות
 
-//   socket.on("disconnect", () => {
-//     console.log("לקוח התנתק");
-//   });
-// });
-// app.get("", (req, res) => {
-//   return res.status(200).json("maching server");
-// });
+// נתיבים לדוגמה
+app.use("/users", require("./src/API/users.routes"));
+app.use("/apartments", require("./src/API/apartments.routes"));
+app.use("/donations", require("./src/API/donations.routes")(io));
+io.on("connection", (socket) => {
+  console.log("לקוח מחובר");
 
-// const PORT = process.env.PORT || 8080;
-// server.listen(PORT, () => {
-//   console.log(`Server is running on http://localhost:${PORT}`);
-// });
+  socket.on("disconnect", () => {
+    console.log("לקוח התנתק");
+  });
+});
+app.get("", (req, res) => {
+  return res.status(200).json("maching server");
+});
+
+const PORT = process.env.PORT || 8080;
+server.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
